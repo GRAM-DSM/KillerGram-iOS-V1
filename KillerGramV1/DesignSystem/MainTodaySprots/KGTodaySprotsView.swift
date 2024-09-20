8c61054da01d7416bb49c01afa4e3eb0cedd12ff
hyeonseog_08 <jeonghyeonseog951@gmail.com>
2024-09-20 16:49:54 +0900

    KGSportsImageView 구현

--- KillerGramV1/DesignSystem/MainTodaySprots/KGTodaySprotsView.swift
+++ KillerGramV1/DesignSystem/MainTodaySprots/KGTodaySprotsView.swift
@@ -2,6 +2,22 @@ import UIKit
 
 class KGTodaySprotsView: UIStackView {
     private let todaySpotsImage = UIImageView(frame: .zero).then {
-        $0.image
+        $0.image = .todaySportsBadminton
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        self.layoutMargins = .init(top: 8, left: 24, bottom: 16, right: 24)
+        self.isLayoutMarginsRelativeArrangement = true
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        [
+            todaySpotsImage
+        ].forEach(self.addArrangedSubview(_:))
     }
 }
