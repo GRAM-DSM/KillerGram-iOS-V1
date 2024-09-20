c059cdab318f594e4a262eb1b74c911d85dc461e
hyeonseog_08 <jeonghyeonseog951@gmail.com>
2024-09-20 16:07:50 +0900

    KGSettingImageView 추가

--- KillerGramV1/DesignSystem/MainTitleView/SettingImage/KGSettingImageView.swift
+++ KillerGramV1/DesignSystem/MainTitleView/SettingImage/KGSettingImageView.swift
@@ -1,1 +1,24 @@
-import Foundation
+import UIKit
+import Then
+
+class KGSettingImageView: UIStackView {
+    private let settingImage = UIImageView(frame: .zero).then {
+        $0.image = .setting
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        self.layoutMargins = .init(top: 12, left: 12, bottom: 12, right: 12)
+        self.isLayoutMarginsRelativeArrangement = true
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        [
+            settingImage
+        ].forEach(self.addArrangedSubview(_:))
+    }
+}
