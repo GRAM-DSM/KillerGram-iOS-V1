26f67b1d03da56f61490aa35a8ba7ca651bc45df
hyeonseog_08 <jeonghyeonseog951@gmail.com>
2024-09-20 15:56:38 +0900

    MainTitleView 생성자 추가

--- KillerGramV1/DesignSystem/MainTitleView/KGMainTitleView.swift
+++ KillerGramV1/DesignSystem/MainTitleView/KGMainTitleView.swift
@@ -1,10 +1,24 @@
 import UIKit
+import Then
 
 class MainTitleView: UIStackView {
     private let logoImageView = UIImageView(frame: .zero).then {
         $0.image = .kgLogo
     }
     
-    private let settingImageView = UIImageView(frame: .zero)
+    private let settingImageView = UIImageView(frame: .zero).then {
+        $0.image = .setting
+    }
     
+    init() {
+        super.init(frame: .zero)
+        self.axis = .vertical
+        self.spacing = 194
+        self.layoutMargins = .init(top: 8, left: 12, bottom: 8, right: 12)
+        self.isLayoutMarginsRelativeArrangement = true
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
