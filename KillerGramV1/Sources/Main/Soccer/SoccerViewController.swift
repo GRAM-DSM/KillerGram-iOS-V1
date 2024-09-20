c253728ef582bf5ba92fe13cc3f1b9e4b2c8ad47
hyeonseog_08 <jeonghyeonseog951@gmail.com>
2024-09-20 20:01:54 +0900

    SoccerView추가

---
+++ KillerGramV1/Sources/Main/Soccer/SoccerViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SoccerViewController.swift
+//  KillerGramV1
+//
+//  Created by 현석의 맥북 on 9/20/24.
+//
+
+import Foundation
---
+++ KillerGramV1/Sources/Main/Soccer/SoccerViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SoccerViewModel.swift
+//  KillerGramV1
+//
+//  Created by 현석의 맥북 on 9/20/24.
+//
+
+import Foundation
