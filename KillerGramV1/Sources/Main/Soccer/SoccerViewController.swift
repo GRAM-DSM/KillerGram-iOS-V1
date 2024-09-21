fb3c52f4ddfc744825d5a4465f4a1bf3ba4ce106
hyeonseog_08 <jeonghyeonseog951@gmail.com>
2024-09-22 00:28:18 +0900

    SoccerViewController 타이틀 추가

--- KillerGramV1/Sources/Main/Soccer/SoccerViewController.swift
+++ KillerGramV1/Sources/Main/Soccer/SoccerViewController.swift
@@ -6,6 +6,11 @@ import RxCocoa
 
 class SoccerViewController: BaseViewController{
     
+    override func attribute() {
+        self.navigationItem.title = "축구"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont.killerGramFont(.semibold, style: .m2), .foregroundColor: UIColor.WHITE]
+    }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
