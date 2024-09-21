d78ba660a65978ca229d64b6e401ef26f22ddfa8
hyeonseog_08 <jeonghyeonseog951@gmail.com>
2024-09-22 00:27:49 +0900

    BaseBallViewController 타이틀 추가

--- KillerGramV1/Sources/Main/Baseball/BaseballViewController.swift
+++ KillerGramV1/Sources/Main/Baseball/BaseballViewController.swift
@@ -6,6 +6,11 @@ import RxCocoa
 
 class BaseballViewController: BaseViewController {
     
+    override func attribute() {
+        self.navigationItem.title = "야구"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont.killerGramFont(.semibold, style: .m2), .foregroundColor: UIColor.WHITE]
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.setNavigationBarHidden(false, animated: animated)
