a86959bd03e9c4fce2c558610ebf85aefb140b17
hyeonseog_08 <jeonghyeonseog951@gmail.com>
2024-09-22 00:26:03 +0900

    PingpongViewController 타이틀 추가

--- KillerGramV1/Sources/Main/Pingpong/PingpongViewController.swift
+++ KillerGramV1/Sources/Main/Pingpong/PingpongViewController.swift
@@ -7,7 +7,8 @@ import RxCocoa
 class PingpongViewController: BaseViewController {
     
     override func attribute() {
-        
+        self.navigationItem.title = "탁구"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont.killerGramFont(.semibold, style: .m2), .foregroundColor: UIColor.WHITE]
     }
     
     override func viewWillAppear(_ animated: Bool) {
