810f60ef13ef5d358a01c9c37c7ffc65e0aba6bf
hyeonseog_08 <jeonghyeonseog951@gmail.com>
2024-09-23 20:08:43 +0900

    myAblityLabel 추가

--- KillerGramV1/DesignSystem/Application/Ablity/KGAbilityView.swift
+++ KillerGramV1/DesignSystem/Application/Ablity/KGAbilityView.swift
@@ -1,1 +1,13 @@
-import Foundation
+import UIKit
+import Then
+
+class KGAbilityView: UIStackView {
+    private let myAblitiLabel = UILabel().then {
+        $0.text = "내 실력"
+        $0.font = .killerGramFont(.semibold, style: .m2)
+    }
+    private let showMyAblituLabel = UILabel().then {
+        
+    }
+    private let changeButotn = UIButton()
+}
