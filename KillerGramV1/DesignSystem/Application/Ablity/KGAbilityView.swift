3b364a42be059d1567122e87e7613d786714cc64
hyeonseog_08 <jeonghyeonseog951@gmail.com>
2024-09-23 20:10:40 +0900

    showMyAblityLabel 추가

--- KillerGramV1/DesignSystem/Application/Ablity/KGAbilityView.swift
+++ KillerGramV1/DesignSystem/Application/Ablity/KGAbilityView.swift
@@ -4,10 +4,15 @@ import Then
 class KGAbilityView: UIStackView {
     private let myAblitiLabel = UILabel().then {
         $0.text = "내 실력"
+        $0.textColor = .WHITE
         $0.font = .killerGramFont(.semibold, style: .m2)
     }
     private let showMyAblituLabel = UILabel().then {
-        
+        $0.text = ""
+        $0.textColor = .MAIN
+        $0.font = .killerGramFont(.regular, style: .m3)
     }
-    private let changeButotn = UIButton()
+    private let changeButotn = UIButton().then {
+        $0.setTitle("변경하기", for: <#T##UIControl.State#>)
+    }
 }
