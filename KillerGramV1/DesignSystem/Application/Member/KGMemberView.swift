e38065f9954a97e3e896af14604c86cab88c5442
hyeonseog_08 <jeonghyeonseog951@gmail.com>
2024-09-24 16:18:29 +0900

    코드 추가

--- KillerGramV1/DesignSystem/Application/Member/KGMemberView.swift
+++ KillerGramV1/DesignSystem/Application/Member/KGMemberView.swift
@@ -2,5 +2,7 @@ import UIKit
 import Then
 
 class KGMemberView: UIStackView {
-    private let memberLabel = KGMemberListView(sumMember: <#T##String#>, countMember: <#T##Int#>)
+    private let memberLabel = KGMemberListView(sumMember: "14", countMember: 2)
+    
+    
 }
