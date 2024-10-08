9c8016fc33b788b2659dc01801155d10992df560
hyeonseog_08 <jeonghyeonseog951@gmail.com>
2024-10-08 19:10:03 +0900

    MemberView 파일 추가

--- KillerGramV1/DesignSystem/SportsState/MemberView.swift
+++ KillerGramV1/DesignSystem/SportsState/MemberView.swift
@@ -1,8 +1,10 @@
+import Foundation
+
 import UIKit
 import SnapKit
 import Then
 
-class KGStateView: UIStackView {
+class MemberView: UIStackView {
     
     private let sportsLabel = UILabel().then {
         $0.font = .killerGramFont(.semibold, style: .m1)
@@ -13,20 +15,18 @@ class KGStateView: UIStackView {
         $0.font = .killerGramFont(.regular, style: .label)
         $0.textColor = .MAIN
     }
-    
-    private let stateLabel = StateView()
-    
+        
     init(sports: String, maxMember: String, nowMember: Int) {
         sportsLabel.attributedText = NSAttributedString(string: sports)
         memberLabel.attributedText = NSAttributedString(string: "\(maxMember)명 중 \(nowMember)명 참여")
         super.init(frame: .zero)
         self.axis = .vertical
-        self.spacing = 163
+        self.spacing = 7
+        self.layoutMargins = .init(top: 0, left: 0, bottom: 0, right: 0)
+        self.isLayoutMarginsRelativeArrangement = true
     }
     
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
-
-
