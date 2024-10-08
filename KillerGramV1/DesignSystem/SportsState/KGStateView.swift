0e01a8b3488bff0e72302c35368a4c7efdeed96b
hyeonseog_08 <jeonghyeonseog951@gmail.com>
2024-10-08 17:13:30 +0900

    운동 종류 열거형 추가

--- KillerGramV1/DesignSystem/SportsState/KGStateView.swift
+++ KillerGramV1/DesignSystem/SportsState/KGStateView.swift
@@ -3,6 +3,7 @@ import SnapKit
 import Then
 
 class KGStateView: UIStackView {
+    
     private let sportsLabel = UILabel().then {
         $0.font = .killerGramFont(.semibold, style: .m1)
         $0.textColor = .WHITE
@@ -14,4 +15,12 @@ class KGStateView: UIStackView {
     }
     
     private let stateLabel = StateView()
+    
+    init(sports: String, maxMember: String, nowMember: Int) {
+        sportsLabel.
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
---
+++ KillerGramV1/DesignSystem/SportsState/Sports.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+public enum KGSports {
+    case soccer
+    case vallyball
+    case basketball
+    case pingpong
+    case badminton
+    case women
+    case baseball
+    case health
+}
