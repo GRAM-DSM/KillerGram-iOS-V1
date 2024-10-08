90ab276d3ee046ff2a5f9187d04b1bdb96ee2a09
hyeonseog_08 <jeonghyeonseog951@gmail.com>
2024-10-08 16:52:30 +0900

    stateLabel 레이아웃

--- KillerGramV1/DesignSystem/SportsState/StateView.swift
+++ KillerGramV1/DesignSystem/SportsState/StateView.swift
@@ -5,5 +5,23 @@ import SnapKit
 class StateView: UIStackView {
     private let stateLabel = UILabel().then {
         $0.text = "완료"
+        $0.font = .killerGramFont(.regular, style: .m3)
+        $0.textColor = .GRAY_600
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        self.layoutMargins = .init(top: 0, left: 0, bottom: 0, right: 0)
+        self.isLayoutMarginsRelativeArrangement = true
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        [
+            stateLabel
+        ].forEach(self.addArrangedSubview(_:))
     }
 }
