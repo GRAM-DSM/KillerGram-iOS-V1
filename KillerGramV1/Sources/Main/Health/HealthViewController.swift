03d2609ab2e3ab5c3de664627b7e32a94994639a
hyeonseog_08 <jeonghyeonseog951@gmail.com>
2024-10-10 15:02:00 +0900

    HealthViewController 완성

--- KillerGramV1/Sources/Main/Health/HealthViewController.swift
+++ KillerGramV1/Sources/Main/Health/HealthViewController.swift
@@ -4,15 +4,20 @@ import SnapKit
 import RxSwift
 import RxCocoa
 
-class HealthViewController: BaseViewController {
-    
+class HealthViewController: SportsViewController {
     override func attribute() {
+        
         self.navigationItem.title = "헬스"
+        
         self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont.killerGramFont(.semibold, style: .m2), .foregroundColor: UIColor.WHITE]
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+        
+        backBarButtonItem.tintColor = .WHITE
+        self.navigationItem.backBarButtonItem = backBarButtonItem
+        
+        self.memberView = KGMemberView(sumMember: "12", countMember: 1)
+        
+        self.ablityView.ablityView.changeButton.rx.tap.subscribe(onNext: {
+            self.navigationController?.pushViewController(HealthAblityViewController(), animated: true)
+        }).disposed(by: disposeBag)
     }
 }
