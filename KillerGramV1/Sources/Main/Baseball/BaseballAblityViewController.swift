c744fa9b24c01c0916979dbcfd2b99bb10b068fc
hyeonseog_08 <jeonghyeonseog951@gmail.com>
2024-10-10 11:24:49 +0900

    BaseballAblityViewController 추가

--- KillerGramV1/Sources/Main/Baseball/BaseballAblityViewController.swift
+++ KillerGramV1/Sources/Main/Baseball/BaseballAblityViewController.swift
@@ -1,8 +1,5 @@
-//
-//  BaseballAblityViewController.swift
-//  KillerGramV1
-//
-//  Created by 현석의 맥북 on 10/10/24.
-//
+import UIKit
 
-import Foundation
+class BaseballAblityViewController: AblityViewController {
+    
+}
