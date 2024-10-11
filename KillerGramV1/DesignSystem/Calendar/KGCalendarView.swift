3041361eb0d9e5beee57c3de93e8ed0d0784e97d
hyeonseog_08 <jeonghyeonseog951@gmail.com>
2024-10-11 22:35:11 +0900

    달력뷰 추가(수정 많이 필요)

--- KillerGramV1/DesignSystem/Calendar/KGCalendarView.swift
+++ KillerGramV1/DesignSystem/Calendar/KGCalendarView.swift
@@ -2,28 +2,52 @@ import UIKit
 import SnapKit
 import Then
 
-class KGCalendarView: UIStackView {
-    private let sorryLabel = UILabel().then {
-        $0.text = "달력 준비중입니다"
-        $0.font = .killerGramFont(.semibold, style: .m3)
-        $0.textColor = .WHITE
-        $0.textAlignment = .center
-        $0.backgroundColor = .GRAY_1100
-    }
+class KGCalendarView: UICollectionView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
+    private let day = ["월", "화", "수", "목", "금"]
+    
+    // MARK: - Initializer
     init() {
-        super.init(frame: .zero)
-        self.layoutMargins = .init(top: 0, left: 0, bottom: 0, right: 0)
-        self.isLayoutMarginsRelativeArrangement = true
+        let layout = UICollectionViewFlowLayout()
+        super.init(frame: .zero, collectionViewLayout: layout)
+        
+        self.delegate = self
+        self.dataSource = self
+        self.register(CustomCell.self, forCellWithReuseIdentifier: CustomCell.identifier)
+        self.backgroundColor = .clear
+        
+        layout.minimumInteritemSpacing = 4
+        layout.minimumLineSpacing = 4
     }
     
-    required init(coder: NSCoder) {
+    required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func layoutSubviews() {
-        [
-            sorryLabel
-        ].forEach(self.addArrangedSubview(_:))
+    // MARK: - UICollectionView DataSource
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return day.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CustomCell.identifier, for: indexPath) as! CustomCell
+        cell.configure(with: day[indexPath.item]) // 요일 텍스트 설정
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegateFlowLayout
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (collectionView.frame.width - 4 * 4) / 5 // 간격을 고려한 셀 너비 계산
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 4
     }
 }
--- KillerGramV1/Sources/Main/Main/MainViewController.swift
+++ KillerGramV1/Sources/Main/Main/MainViewController.swift
@@ -104,3 +104,4 @@ class MainViewController: BaseViewController {
         }
     }
 }
+
