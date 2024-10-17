808ae512ab1c4b8a1af8a8ddf6cbcf13a17c7f55
hyeonseog_08 <jeonghyeonseog951@gmail.com>
2024-10-17 10:55:01 +0900

    오류수정

--- KillerGramV1/Sources/Main/Soccer/SoccerAblityViewController.swift
+++ KillerGramV1/Sources/Main/Soccer/SoccerAblityViewController.swift
@@ -3,5 +3,32 @@ import UIKit
 class SoccerAblityViewController: AblityViewController {
     override func attribute() {
         self.navigationItem.title = "축구"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont.killerGramFont(.semibold, style: .m2), .foregroundColor: UIColor.WHITE]
+        
+        backBarButtonItem.tintColor = .WHITE
+        self.navigationItem.backBarButtonItem = backBarButtonItem
+        
+        highButton.setAttributedTitle(viewModel.highAttributedstr, for: .normal)
+        middleButton.setAttributedTitle(viewModel.middleAttributedstr, for: .normal)
+        lowButton.setAttributedTitle(viewModel.lowAttributedstr, for: .normal)
+        
+        
+        self.highButton.rx.tap.subscribe(onNext: {
+            self.highButtonDidTap()
+        }).disposed(by: disposeBag)
+        
+        self.middleButton.rx.tap.subscribe(onNext: {
+            self.middleButtonDidTap()
+        }).disposed(by: disposeBag)
+        
+        self.lowButton.rx.tap.subscribe(onNext: {
+            self.lowButtonDidTap()
+        }).disposed(by: disposeBag)
+        
+        self.completeButton.rx.tap.subscribe(onNext: {
+            self.ablityView.showMyAblityLabel.text = self.ablity
+            self.ablityView.showMyAblityLabel.setNeedsDisplay()
+            self.navigationController?.popViewController(animated: true)
+        }).disposed(by: disposeBag)
     }
 }
