ccb8f48651df204e15f6f61b930746ca351dca23
hyeonseog_08 <jeonghyeonseog951@gmail.com>
2024-10-21 16:36:42 +0900

    settingViewController title 추가

--- KillerGramV1/Sources/Main/setting/settingViewController.swift
+++ KillerGramV1/Sources/Main/setting/settingViewController.swift
@@ -1,5 +1,18 @@
 import UIKit
 
 class settingViewController: BaseViewController {
+    let backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: self, action: nil)
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    
+    override func attribute() {
+        self.navigationItem.title = "설정"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont.killerGramFont(.semibold, style: .m2), .foregroundColor: UIColor.WHITE]
+        
+        backBarButtonItem.tintColor = .WHITE
+    }
 }
